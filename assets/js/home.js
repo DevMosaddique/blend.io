958c4be30586ed8f2cfbf727d5f56de3366d2adc
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-12-18 18:12:42 +0000

    update

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,19 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
-
 "use strict";
+
+const $searchField = document.querySelector("[data-search-field]");
+const $searchBtn = document.querySelector("[data-search-btn]");
+
+$searchBtn.addEventListener("click", function () {
+  if ($searchField.value)
+    window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+$searchField.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") $searchBtn.click();
+});
+
+const $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let [$lastActiveTabPanel] = $tabPanels;
+let [$lastActiveTabBtn] = $tabBtns;
